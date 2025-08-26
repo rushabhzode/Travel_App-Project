8e14f2b3d8b53063282be3189546472b724e080c
rushabh <rushabhzode84@gmail.com>
2025-08-26 14:50:43 +0530

    updating footer,navbar file

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (
+    <h1 className="text-bold text-teal-500 text-2xl">Hello World</h1>
+  );
+}
+
---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <nav className = "border-2 border-red-500 flexbetween max-container padding-container relative z-30 py-5"></nav> 
+  
+  )
+}
+
+export default Navbar
